416f70df0bb287c4ad593ebd5a88904ab79a7882
fangzhoui <601064569@qq.com>
2017-07-09 22:52:36 +0800

    add new-1

--- newP/src/App.vue
+++ newP/src/App.vue
@@ -1,28 +1,42 @@
 <template>
-  <div id="app">
-    <keep-alive>
-      <router-view></router-view>
-    </keep-alive>
+  <div>
+    <div class="header">
+      
+    </div>
+    <div class="wrap">
+      <ul>
+        <li>
+          <v-welcome></v-welcome>
+        </li>
+        <li>
+          <v-pic></v-pic>
+        </li>
+      </ul>
+    </div>
     <div class="footer">
       <a href="javascript:;">
-        <router-link  to="/{linkTo}">
-          <input class="btn" type="button" value="下一步">
-        </router-link>
+        <input class="btn" type="button" value="下一步">
       </a>
     </div>
   </div>
 </template>
 
-<script>
-export default {
-  data () {
-    return {
-      
+<script type="text/ecmascript-6">
+  import welcome from './components/welcome/welcome'
+  import pic from './components/pic/pic'
+  export default{
+    data () {
+      return {}
+    },
+    components: {
+      'v-welcome': welcome,
+      'v-pic': pic
     }
   }
-}
 </script>
 
 <style>
+.wrap{
 
+}
 </style>
---
+++ newP/src/components/welcome/welcome.vue
@@ -0,0 +1,52 @@
+<template>
+<div class="welcome">
+	<div class="pic_wrap">
+	</div>
+	<div class="text_wrap">
+		<div class="hr_18"></div>
+		<p class="name">余聪</p>
+		<div class="hr_10"></div>
+		<p class="name">欢迎你,&nbsp公司的</p>
+		<div class="hr_10"></div>
+		<p class="num">第100号</p>
+		<div class="hr_10"></div>
+		<p class="name">成员,&nbsp我们的伙伴</p>
+		<div class="hr_10"></div>
+		<p class="name sm">——杭州开启网络科技有限公司</p>
+	</div>
+</div>
+</template>
+
+<script type="text/ecmascript-6">
+export default{}
+</script>
+
+<style>
+/* 文字部分 */
+.welcome {
+  padding: 0.62rem 1.68rem;
+}
+.welcome .pic_wrap {
+  width: 4.16rem;
+  height: 2.92rem;
+  background: url('./icon_index_bgch.jpg');
+  background-size: 100% 100%;
+}
+.welcome .text_wrap {
+  text-align: center;
+  font-size: 0.36rem;
+  color: #222222;
+}
+.welcome .text_wrap .name {
+  line-height: 0.4rem;
+}
+.welcome .text_wrap .num {
+  font-size: 0.96rem;
+  color: #FF5E3B;
+  line-height: 1;
+}
+.welcome .text_wrap .sm {
+  font-size: 0.28rem;
+}
+
+</style>
